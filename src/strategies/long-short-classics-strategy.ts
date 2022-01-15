ded1a7ffe375117bc825f39cb129a00a91366ef6
Michael <michael.spengler@sap.com>
2022-01-15 20:00:21 +0100

    f

--- src/strategies/long-short-classics-strategy.ts
+++ src/strategies/long-short-classics-strategy.ts
@@ -162,13 +162,13 @@ export abstract class LongShortClassics extends VoFarmStrategy {
         console.log(`longHighestSinceX: ${longHighestSinceX}`)
         console.log(`shortHighestSinceX: ${shortHighestSinceX}`)
 
-        if (longLowestSinceX >= 11) {
+        if (longLowestSinceX >= 50) {
 
             const reason = `we enhance our ${assetInfo.pair} long position (lowestSinceX: ${longLowestSinceX} - (${longPosition.data.unrealised_pnl})) by ${assetInfo.minTradingAmount}`
             this.addInvestmentAdvice(Action.BUY, assetInfo.minTradingAmount, assetInfo.pair, reason)
         }
 
-        if (shortLowestSinceX >= 11) {
+        if (shortLowestSinceX >= 50) {
 
             const reason = `we enhance our ${assetInfo.pair} short position (shortLowestSinceX: ${shortLowestSinceX} (${shortPosition.data.unrealised_pnl})) by ${assetInfo.minTradingAmount}`
             this.addInvestmentAdvice(Action.SELL, assetInfo.minTradingAmount, assetInfo.pair, reason)
